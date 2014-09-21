381a05c26e4bb1b7c4898e4b22d71a7e45a6d172
Fabio Chicout <fabio.cesar.medeiros@gmail.com>
2014-09-20 22:51:14 -0300

    Removing old folders

--- draw_sprites/main.lua
+++
@@ -1,6 +0,0 @@
-
-function love.draw()
-	fullImg = love.graphics.newImage("assets/char.bmp")
-	sprite = love.graphics.newQuad(0, 0, 60, 60, fullImg:getDimensions())
-	love.graphics.draw(fullImg, sprite, 50, 50)
-end
